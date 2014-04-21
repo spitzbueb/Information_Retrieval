05bc0274088e0b76676e91bc4c99024fa6354012
Philipp Schalcher <philipp.schalcher@gmail.com>
2014-04-21 15:40:39 +0200

    GUI angefangen

--- Search_Engine/src/ch/phischa/Main.java
+++ Search_Engine/src/ch/phischa/Main.java
@@ -1,8 +1,13 @@
 package ch.phischa;
 
 public class Main {
-	public Main()
-	{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		new GUI();
 	}
+
 }
