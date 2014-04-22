561731c461eb1bf2ffa8fb493b94f48a812539ad
Philipp Schalcher <philipp.schalcher@gmail.com>
2014-04-22 16:33:02 +0200

    Indexer erstellt

--- Search_Engine/src/ch/phischa/GUI.java
+++ Search_Engine/src/ch/phischa/GUI.java
@@ -1,11 +1,13 @@
 package ch.phischa;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -13,8 +15,11 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class GUI {
-
+	
 	private JFrame mainFrame;
+	private JTextField suche, pfadFeld;
+	private JButton sucheButton, indexButton;
+	private JLabel dauer, dauerTitel;
 	
 	public GUI()
 	{
@@ -25,21 +30,49 @@ public class GUI {
 	{
 		mainFrame = new JFrame("Lucene Search Engine");
 		JMenuBar menubar = createMenuBar();
-		Container pane = mainFrame.getContentPane();
-		pane.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
 		
-		JTextField suche = new JTextField(20);
-		JButton sucheButton = new JButton("Suche");
+		JLabel sucheLabel = new JLabel("Suche:");
+		JLabel indexLabel = new JLabel("Pfad:");
+		suche = new JTextField(20);
+		sucheButton = new JButton("Suche");
+		dauer = new JLabel();
+		dauerTitel = new JLabel("Dauer:");
+		pfadFeld = new JTextField(20);
+		indexButton = new JButton("Index");
 
+		JPanel northPanel = new JPanel();
+		JPanel centerPanel = new JPanel();
+		JPanel southPanel = new JPanel();
+		northPanel.add(sucheLabel);
+		northPanel.add(suche);
+		northPanel.add(sucheButton);
+		centerPanel.add(dauerTitel);
+		centerPanel.add(dauer);
+		southPanel.add(indexLabel);
+		southPanel.add(pfadFeld);
+		southPanel.add(indexButton);
 		
+		mainFrame.add(northPanel,BorderLayout.NORTH);
+		mainFrame.add(centerPanel,BorderLayout.CENTER);
+		mainFrame.add(southPanel,BorderLayout.SOUTH);
 				
 		mainFrame.setJMenuBar(menubar);
 		
 		mainFrame.pack();
-		mainFrame.setSize(300,400);
+		mainFrame.setSize(700,800);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setVisible(true);
+		
+		indexButton.addActionListener(new ActionListener()
+		{
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				new Index(pfadFeld.getText().toString());
+			}
+			
+		});
 	}
 	
 	private JMenuBar createMenuBar()
